3e4edcf901d03a982fa05e642215580cdbe66bf8
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-02-09 21:40:03 +0000

    Automatically backed up by Learn

--- lib/models/musiclibrarycontroller.rb
+++ lib/models/musiclibrarycontroller.rb
@@ -112,7 +112,6 @@ class MusicLibraryController
       if (1..Song.all.length).include?(input)
       song = list_of_songs[input+2]
       puts "Playing #{song.name} by #{song.artist.name}"
-    
       end
   end
 
