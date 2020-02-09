b98b21b70cedf08f54aab60b60ed2463fa465504
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-02-09 21:45:24 +0000

    Automatically backed up by Learn

--- lib/models/musiclibrarycontroller.rb
+++ lib/models/musiclibrarycontroller.rb
@@ -110,9 +110,10 @@ class MusicLibraryController
 
     input = gets.strip.to_i
       if (1..Song.all.length).include?(input)
+        binding.pry 
       song = list_of_songs[input+2]
       puts "Playing #{song.name} by #{song.artist.name}"
-      binding.pry 
+      
       end
   end
 
